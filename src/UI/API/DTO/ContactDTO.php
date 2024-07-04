fa6bff5c8632178de980490e91a40f46709c832e
Anastas Mironov <ast@l80.ru>
2024-07-05 02:01:55 +0900

    fix: laravel-data version ^4.5

--- src/UI/API/DTO/ContactDTO.php
+++ src/UI/API/DTO/ContactDTO.php
@@ -3,10 +3,13 @@
 namespace AdminKit\Contacts\UI\API\DTO;
 
 use AdminKit\Contacts\Models\Contact;
+use Spatie\LaravelData\Concerns\WithDeprecatedCollectionMethod;
 use Spatie\LaravelData\Data;
 
 class ContactDTO extends Data
 {
+    use WithDeprecatedCollectionMethod;
+
     public function __construct(
         public string $title,
         public string $phone,
